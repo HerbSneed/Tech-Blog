5ab57dc93178d7b6478b19841173da03ba485872
Herb Sneed <hmsneed@gmail.com>
2023-07-25 14:04:03 -0400

    adding headers

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,6 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  // Running locally or on another environment
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
